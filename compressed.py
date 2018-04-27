bcdb551f3d1a5a02630b20bd72cc7140b852ebc0
JermyJoy <35697388+JermyJoy@users.noreply.github.com>
2018-04-27 13:46:32 -0400

    Update compressed.py

--- compressed.py
+++ compressed.py
@@ -2,20 +2,17 @@ import os
 import sys
 import csv 
 import matplotlib.pyplot as plt
+import numpy as np
         #####################################################################
 def main():
-        month = []; # 7
-        meta = []; # 4 
-        name = []; # 0
-        prod = []; # 1
-        age = []; # 2
-        imdb = []; # 3
-        num_votes = []; # 5
-        day = []; # 6
-        year = []; # 8
-        budget = []; # 9
-        box_office = []; # 10
-        multiplier = []; # 11
+        name = []
+        prod = []
+        age = []
+        imdb = []
+        day = []
+        month = []
+        year = []
+        multiplier = []
         #####################################################################
         print "starting..."
         with open('full_dataset_cleaned.csv') as csv_file:
@@ -25,57 +22,53 @@ def main():
                         prod.append(row[1])
                         age.append(row[2])
                         imdb.append(row[3])
-                        meta.append(row[4])
-                        num_votes.append(row[5])
                         day.append(row[6])
                         month.append(row[7])
                         year.append(row[8])
-                        budget.append(row[9])
-                        box_office.append(row[10])
-                        multiplier.append(row[11])
+                        multiplier.append(row[11]) 
         ######################################################################
         Jans = [index for index in range(1,len(month)) if month[index] == 'Jan']
         Febs = [index for index in range(1,len(month)) if month[index] == 'Feb']
-        Mar = [index for index in range(1,len(month)) if month[index] == 'Mar']
-        Apr = [index for index in range(1,len(month)) if month[index] == 'Apr']
+        Mar  = [index for index in range(1,len(month)) if month[index] == 'Mar']
+        Apr  = [index for index in range(1,len(month)) if month[index] == 'Apr']
         May  = [index for index in range(1,len(month)) if month[index] == 'May']
         June = [index for index in range(1,len(month)) if month[index] == 'Jun']
         July = [index for index in range(1,len(month)) if month[index] == 'Jul']
-        Aug = [index for index in range(1,len(month)) if month[index] == 'Aug']
-        Sep = [index for index in range(1,len(month)) if month[index] == 'Sep']
-        Oct = [index for index in range(1,len(month)) if month[index] == 'Oct']
-        Nov = [index for index in range(1,len(month)) if month[index] == 'Nov']
-        Dec = [index for index in range(1,len(month)) if month[index] == 'Dec']
+        Aug  = [index for index in range(1,len(month)) if month[index] == 'Aug']
+        Sep  = [index for index in range(1,len(month)) if month[index] == 'Sep']
+        Oct  = [index for index in range(1,len(month)) if month[index] == 'Oct']
+        Nov  = [index for index in range(1,len(month)) if month[index] == 'Nov']
+        Dec  = [index for index in range(1,len(month)) if month[index] == 'Dec']
         NONE = [index for index in range(1,len(month)) if month[index] == 'N/A']
         # There are over 200 entries that are N/A !
         #####################################################################
         multiplier_Jans = [float(multiplier[val]) for val in Jans]
-        multiplier_Feb = [float(multiplier[val]) for val in Febs]
-        multiplier_Mar = [float(multiplier[val]) for val in Mar]
-        multiplier_Apr = [float(multiplier[val]) for val in Apr]
-        multiplier_May = [float(multiplier[val]) for val in May]
+        multiplier_Feb  = [float(multiplier[val]) for val in Febs]
+        multiplier_Mar  = [float(multiplier[val]) for val in Mar]
+        multiplier_Apr  = [float(multiplier[val]) for val in Apr]
+        multiplier_May  = [float(multiplier[val]) for val in May]
         multiplier_June = [float(multiplier[val]) for val in June]
         multiplier_July = [float(multiplier[val]) for val in July]
-        multiplier_Aug = [float(multiplier[val]) for val in Aug]
-        multiplier_Sep = [float(multiplier[val]) for val in Sep]
-        multiplier_Oct = [float(multiplier[val]) for val in Oct]
-        multiplier_Nov = [float(multiplier[val]) for val in Nov]
-        multiplier_Dec = [float(multiplier[val]) for val in Dec]
+        multiplier_Aug  = [float(multiplier[val]) for val in Aug]
+        multiplier_Sep  = [float(multiplier[val]) for val in Sep]
+        multiplier_Oct  = [float(multiplier[val]) for val in Oct]
+        multiplier_Nov  = [float(multiplier[val]) for val in Nov]
+        multiplier_Dec  = [float(multiplier[val]) for val in Dec]
         months = [1,2,3,4,5,6,7,8,9,10,11,12]
         #### Averages for the months and their respective multipliers #####
         avg_months = [];
-        avg_months.append(sum(multiplier_Jans)/ (len(multiplier_Jans)))
-        avg_months.append(sum(multiplier_Feb)/ (len(multiplier_Feb)))
-        avg_months.append(sum(multiplier_Mar)/ (len(multiplier_Mar)))
-        avg_months.append(sum(multiplier_Apr)/ (len(multiplier_Apr)))
-        avg_months.append(sum(multiplier_May)/ (len(multiplier_May)))
-        avg_months.append(sum(multiplier_June)/ (len(multiplier_June)))
-        avg_months.append(sum(multiplier_July)/ (len(multiplier_July)))
-        avg_months.append(sum(multiplier_Aug)/ (len(multiplier_Aug)))
-        avg_months.append(sum(multiplier_Sep)/ (len(multiplier_Sep)))
-        avg_months.append(sum(multiplier_Oct)/ (len(multiplier_Oct)))
-        avg_months.append(sum(multiplier_Nov)/ (len(multiplier_Nov)))
-        avg_months.append(sum(multiplier_Dec)/ (len(multiplier_Dec)))
+        avg_months.append(sum(multiplier_Jans) / (len(multiplier_Jans)))
+        avg_months.append(sum(multiplier_Feb)  / (len(multiplier_Feb)))
+        avg_months.append(sum(multiplier_Mar)  / (len(multiplier_Mar)))
+        avg_months.append(sum(multiplier_Apr)  / (len(multiplier_Apr)))
+        avg_months.append(sum(multiplier_May)  / (len(multiplier_May)))
+        avg_months.append(sum(multiplier_June) / (len(multiplier_June)))
+        avg_months.append(sum(multiplier_July) / (len(multiplier_July)))
+        avg_months.append(sum(multiplier_Aug)  / (len(multiplier_Aug)))
+        avg_months.append(sum(multiplier_Sep)  / (len(multiplier_Sep)))
+        avg_months.append(sum(multiplier_Oct)  / (len(multiplier_Oct)))
+        avg_months.append(sum(multiplier_Nov)  / (len(multiplier_Nov)))
+        avg_months.append(sum(multiplier_Dec)  / (len(multiplier_Dec)))
         ###################################################################
         plt.figure(1)
         plt.clf
@@ -86,11 +79,12 @@ def main():
         #plt.show()
         #####################################################################
         avg_seasons = []
-        avg_seasons.append((avg_months[5]+avg_months[6]+avg_months[7]) / 3) # Summer
-        avg_seasons.append((avg_months[8]+avg_months[9]+avg_months[10]) / 3) # Fall
-        avg_seasons.append((avg_months[2]+avg_months[3]+avg_months[4]) / 3) # Spring
-        avg_seasons.append((avg_months[0]+avg_months[1]+avg_months[11]) / 3) # Winter
-        avg_seasons.append((avg_months[5]+avg_months[6]+avg_months[7]) / 3) # Summer
+        season_divison = 12/4
+        avg_seasons.append((avg_months[5]+avg_months[6]+avg_months[7]) / season_divison) # Summer
+        avg_seasons.append((avg_months[8]+avg_months[9]+avg_months[10]) / season_divison) # Fall
+        avg_seasons.append((avg_months[2]+avg_months[3]+avg_months[4]) / season_divison) # Spring
+        avg_seasons.append((avg_months[0]+avg_months[1]+avg_months[11]) / season_divison) # Winter
+        avg_seasons.append((avg_months[5]+avg_months[6]+avg_months[7]) / season_divison) # Summer
         season = [1,2,3,4,5] # Summer -> Fall -> Spring -> Winter
         #####################################################################
         plt.figure(2)
@@ -101,18 +95,18 @@ def main():
         plt.title('Multiplier V.S. Season')
         #plt.show()
         #####################################################################
-        rating_Jan = [float(imdb[val]) for val in Jans if imdb[val] != '']
-        rating_Feb = [float(imdb[val]) for val in Febs if imdb[val] != '']
-        rating_Mar = [float(imdb[val]) for val in Mar if imdb[val] != '']
-        rating_Apr = [float(imdb[val]) for val in Apr if imdb[val] != '']
-        rating_May = [float(imdb[val]) for val in May if imdb[val] != '']
+        rating_Jan  = [float(imdb[val]) for val in Jans if imdb[val] != '']
+        rating_Feb  = [float(imdb[val]) for val in Febs if imdb[val] != '']
+        rating_Mar  = [float(imdb[val]) for val in Mar if imdb[val] != '']
+        rating_Apr  = [float(imdb[val]) for val in Apr if imdb[val] != '']
+        rating_May  = [float(imdb[val]) for val in May if imdb[val] != '']
         rating_June = [float(imdb[val]) for val in June if imdb[val] != '']
         rating_July = [float(imdb[val]) for val in July if imdb[val] != '']
-        rating_Aug = [float(imdb[val]) for val in Aug if imdb[val] != '']
-        rating_Sep = [float(imdb[val]) for val in Sep if imdb[val] != '']
-        rating_Oct = [float(imdb[val]) for val in Oct if imdb[val] != '']
-        rating_Nov = [float(imdb[val]) for val in Nov if imdb[val] != '']
-        rating_Dec = [float(imdb[val]) for val in Dec if imdb[val] != '']
+        rating_Aug  = [float(imdb[val]) for val in Aug if imdb[val] != '']
+        rating_Sep  = [float(imdb[val]) for val in Sep if imdb[val] != '']
+        rating_Oct  = [float(imdb[val]) for val in Oct if imdb[val] != '']
+        rating_Nov  = [float(imdb[val]) for val in Nov if imdb[val] != '']
+        rating_Dec  = [float(imdb[val]) for val in Dec if imdb[val] != '']
         #####################################################################
         avg_rating = [];
         avg_rating.append(sum(rating_Jan)/len(rating_Jan))
@@ -134,7 +128,48 @@ def main():
         plt.ylabel('Multiplier (Budget/Box Office)')
         plt.xlabel('Month [Jan-Dec]')
         plt.title('Rating V.S. Month')
+        #plt.show()
+        #####################################################################
+        ratings = [float(imdb[val]) for val in range(1,len(imdb)) if imdb[val] != '']
+        lowest_rating         = [val for val in range(1,len(ratings)) if ratings[val] <= 2]
+        budget_lowest_rating  = [float(multiplier[val]) for val in lowest_rating]
+        lower_rating          = [val for val in range(1,len(ratings)) if ratings[val] > 2 and ratings[val] <= 4]
+        budget_lower_rating   = [float(multiplier[val]) for val in lower_rating]
+        mid_rating            = [val for val in range(1,len(ratings)) if ratings[val] > 4 and ratings[val] <= 6]
+        budget_mid_rating     = [float(multiplier[val]) for val in mid_rating]
+        higher_rating         = [val for val in range(1,len(ratings)) if ratings[val] > 6 and ratings[val] <= 8]
+        budget_higher_rating  = [float(multiplier[val]) for val in higher_rating]
+        highest_rating        = [val for val in range(1,len(ratings)) if ratings[val] > 8 and ratings[val]<= 10]
+        budget_highest_rating = [float(multiplier[val]) for val in highest_rating]
+        avg_budget = [np.mean(budget_lowest_rating),np.mean(budget_lower_rating),np.mean(budget_mid_rating),np.mean(budget_higher_rating),np.mean(budget_highest_rating)]
+        #####################################################################
+        plt.figure(4)
+        plt.clf
+        plt.plot([1,2,3,4,5],avg_budget)
+        plt.title('Movie rating V.S. Average Multiplier')
+        plt.xlabel('Rating {(0-2)=1, (2-4)=2, (4-6)=3, (6-8)=4, (8-10)=5})')
+        plt.ylabel('Average Multiplier (Budget / Box Office)')
+        #plt.show()        
+        #####################################################################
+        winter = (rating_Jan + rating_Dec + rating_Feb)
+        winter_rating = np.mean([val for val in winter])
+        spring = (rating_Mar + rating_Apr + rating_May)
+        spring_rating = np.mean([val for val in spring])
+        fall   = (rating_Sep + rating_Oct + rating_Nov)
+        fall_rating = np.mean([val for val in fall])
+        summer = (rating_June + rating_July + rating_Aug)
+        summer_rating = np.mean([val for val in summer])
+        avg_season_rating = [summer_rating,fall_rating,winter_rating,spring_rating]
+        #####################################################################
+        plt.figure(5)
+        plt.clf
+        plt.plot([1,2,3,4],avg_season_rating)
+        plt.xlabel('Seasons {1=Summer, 2=Fall, 3=Winter, 4=Spring}')
+        plt.ylabel('Average IMDB Rating [0-10]')
+        plt.title('Season V.S. IMDB Rating')
         plt.show()
+        
+        
         
 if __name__ == '__main__':
         main();
